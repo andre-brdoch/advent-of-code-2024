8f467b5de902e9eae887528b39c8624bd17c2ab1
andre-brdoch <31482129+andre-brdoch@users.noreply.github.com>
2024-12-01 09:20:22 +0100

    refactor: Update template

--- src/day-xx/test.ts
+++ src/day-xx/test.ts
@@ -1,43 +1,49 @@
 import { describe, it } from 'node:test'
 import assert from 'node:assert'
-import { getSum, parseFile, solvePt1, solvePt2 } from './solution'
+import { parseFile } from './solution'
 import { getTodaysData } from '../utils/read-data'
 
-describe('day-xx', () => {
-  describe('utils', () => {
-    it('parses data', async () => {
-      const input = await getTodaysData('dev-input.txt')
-      const result = parseFile(input)
-      assert.deepEqual(result, [1, 2])
-    })
+describe('day-xx', async () => {
+  const [
+    inputExample,
+    // inputReal
+  ] = await Promise.all([getTodaysData('example-input.txt'), getTodaysData('real-input.txt')])
 
-    it('calculates sums', () => {
-      const result = getSum([2, 5, 11])
-      assert.strictEqual(result, 18)
-    })
-  })
-
-  describe('example data', async () => {
-    const input = await getTodaysData('example-input.txt')
-
-    it('solves pt 1', () => {
-      const result = solvePt1(input)
-      assert.strictEqual(result, 6)
-    })
-
-    it('solves pt 2', () => {
-      const result = solvePt2(input)
-      assert.strictEqual(result, 12)
+  describe('helpers', () => {
+    it('parseFile()', () => {
+      const result = parseFile(inputExample)
+      const expected = undefined
+      assert.deepEqual(result, expected)
     })
   })
 
-  describe('real data', async () => {
-    const input = await getTodaysData('real-input.txt')
+  // describe('part 1', () => {
+  //   it('example data', () => {
+  //     const result = solvePt1(inputExample)
+  //     const expected = undefined
+  //     assert.strictEqual(result, expected)
+  //   })
 
-    const result1 = solvePt1(input)
-    console.log(`=== Result pt. 1: ${result1} ===`)
+  //   // it('real data', () => {
+  //   //   const result = solvePt1(inputReal)
+  //   //   console.log(`=== Result pt. 1: ${result} ===`)
+  //   //   const expected = undefined
+  //   //   assert.strictEqual(result, expected)
+  //   // })
+  // })
 
-    const result2 = solvePt2(input)
-    console.log(`=== Result pt. 2: ${result2} ===`)
-  })
+  // describe('part 2', () => {
+  //   it('example data', () => {
+  //     const result = solvePt2(inputExample)
+  //     const expected = undefined
+  //     assert.strictEqual(result, expected)
+  //   })
+
+  //   // it('real data', () => {
+  //   //   const result = solvePt2(inputReal)
+  //   //   console.log(`=== Result pt. 2: ${result} ===`)
+  //   //   const expected = undefined
+  //   //   assert.strictEqual(result, expected)
+  //   // })
+  // })
 })
