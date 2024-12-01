50e752169df29712a8ee997d85b587f7ccba7a9d
andre-brdoch <31482129+andre-brdoch@users.noreply.github.com>
2024-12-01 07:12:27 +0100

    feat(01): Pair columns

--- src/day-01/solution.ts
+++ src/day-01/solution.ts
@@ -8,6 +8,23 @@ export function solvePt1(input: string): any {
 //   const parsed = parseFile(input)
 // }
 
+export function getDistances(columns: Columns): number[] {
+  const [left, right] = sortColumns(columns)
+  return left.map((lc) => {
+    const [lower, higher] = lc.sort()
+    return higher - lower
+  })
+}
+
+export function pairColumns(columns: Columns): number[] {
+  const [colLeft, colRight] = sortColumns(columns)
+  return colLeft.map((_, i) => [colLeft[i], colRight[i]])
+}
+
+function sortColumns(columns: Columns): Columns {
+  return columns.map((c) => c.sort())
+}
+
 export function parseFile(file: string): Columns {
   const startVal: Columns = [[], []]
   return file.split('\n').reduce(([resultLeft, resultRight], line) => {
--- src/day-01/test.ts
+++ src/day-01/test.ts
@@ -1,24 +1,43 @@
 import { describe, it } from 'node:test'
 import assert from 'node:assert'
-import { parseFile, solvePt1, solvePt2 } from './solution'
+import { getDistances, pairColumns, parseFile, solvePt1, solvePt2 } from './solution'
 import { getTodaysData } from '../utils/read-data'
 
 describe('day-xx', () => {
   describe('utils', () => {
-    it('parses data', async () => {
-      const input = await getTodaysData('example-input.txt')
-      console.log(input)
+    describe('parseFile()', () => {
+      it('parses data', async () => {
+        const input = await getTodaysData('example-input.txt')
+        console.log(input)
 
-      const result = parseFile(input)
-      assert.deepEqual(result, [
-        [3, 4, 2, 1, 3, 3],
-        [4, 3, 5, 3, 9, 3],
-      ])
+        const result = parseFile(input)
+        assert.deepEqual(result, [
+          [3, 4, 2, 1, 3, 3],
+          [4, 3, 5, 3, 9, 3],
+        ])
+      })
     })
 
-    // it('calculates sums', () => {
-    //   const result = getSum([2, 5, 11])
-    //   assert.strictEqual(result, 18)
+    describe('pairColumns()', () => {
+      it('pairs columns', () => {
+        const result = pairColumns([
+          [1, 2, 7],
+          [4, 1, 3],
+        ])
+        assert.deepEqual(result, [
+          [1, 1],
+          [2, 3],
+          [7, 4],
+        ])
+      })
+    })
+
+    // it('getDistances()', () => {
+    //   const result = getDistances([
+    //     [1, 2, 7],
+    //     [4, 1, 3],
+    //   ])
+    //   assert.strictEqual(result, [0, 1, 3])
     // })
   })
 
