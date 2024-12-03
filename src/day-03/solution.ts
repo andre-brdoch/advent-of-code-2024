b96ca5ad1b9b40f2e5e838674b1af08b44a6a3cb
andre.brdoch <31482129+andre-brdoch@users.noreply.github.com>
2024-12-03 07:35:43 +0100

    feat: Solve pt 1

--- src/day-03/solution.ts
+++ src/day-03/solution.ts
@@ -1,9 +1,17 @@
-export function solvePt1(input: string): any {
+import { getSum } from '../utils/array'
+
+export function solvePt1(input: string): number {
   const parsed = parseFile(input)
+  return sumMul(parsed)
 }
 
 export function solvePt2(input: string): any {
   const parsed = parseFile(input)
+}
+
+export function sumMul(mulStrings: string[]): number {
+  const multiplied = mulStrings.map(mul)
+  return getSum(multiplied)
 }
 
 export function mul(mulString: string): number {
--- src/day-03/test.ts
+++ src/day-03/test.ts
@@ -1,6 +1,6 @@
 import { describe, it } from 'node:test'
 import assert from 'node:assert'
-import { mul, parseFile } from './solution'
+import { mul, parseFile, solvePt1 } from './solution'
 import { InputReader } from '../utils/InputReader'
 import consola from 'consola'
 
@@ -20,20 +20,20 @@ describe('day-03', async () => {
     })
   })
 
-  // describe('part 1', () => {
-  //   it('example data', () => {
-  //     const result = solvePt1(inputExample)
-  //     const expected = undefined
-  //     assert.strictEqual(result, expected)
-  //   })
+  describe('part 1', () => {
+    it('example data', () => {
+      const result = solvePt1(inputExample)
+      const expected = 161
+      assert.strictEqual(result, expected)
+    })
 
-  //   // it('real data', () => {
-  //   //   const result = solvePt1(inputReal)
-  //   //   consola.success(`=== Result pt. 1: ${result} ===`)
-  //   //   const expected = undefined
-  //   //   assert.strictEqual(result, expected)
-  //   // })
-  // })
+    it('real data', () => {
+      const result = solvePt1(inputReal)
+      consola.success(`=== Result pt. 1: ${result} ===`)
+      const expected = 188741603
+      assert.strictEqual(result, expected)
+    })
+  })
 
   // describe('part 2', () => {
   //   it('example data', () => {
