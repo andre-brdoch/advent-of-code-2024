774cf84950178377be79d59cb025e8bc892d660b
andre.brdoch <31482129+andre-brdoch@users.noreply.github.com>
2024-12-08 20:47:31 +0100

    feat: Parse file

--- src/day-08/solution.ts
+++ src/day-08/solution.ts
@@ -1,18 +1,11 @@
 export function solvePt1(input: string): any {
   const parsed = parseFile(input)
-  return
 }
 
 export function solvePt2(input: string): any {
   const parsed = parseFile(input)
-  return
 }
 
 export function parseFile(file: string): any {
-  return file
-    .split('\n')
-    .filter((line) => line)
-    .map((line) => {
-      //
-    })
+  return file.split('\n').map((line) => line.split(''))
 }
