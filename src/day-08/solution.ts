8faa31c55b246ba9830dcd072a356eb7a7435a00
andre.brdoch <31482129+andre-brdoch@users.noreply.github.com>
2024-12-08 17:19:58 +0100

    feat: Create day 08

---
+++ src/day-08/solution.ts
@@ -0,0 +1,18 @@
+export function solvePt1(input: string): any {
+  const parsed = parseFile(input)
+  return
+}
+
+export function solvePt2(input: string): any {
+  const parsed = parseFile(input)
+  return
+}
+
+export function parseFile(file: string): any {
+  return file
+    .split('\n')
+    .filter((line) => line)
+    .map((line) => {
+      //
+    })
+}
