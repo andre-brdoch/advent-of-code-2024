2f441b61d89c53809634898365afa39e23432feb
andre.brdoch <31482129+andre-brdoch@users.noreply.github.com>
2024-12-08 21:41:15 +0100

    feat: Solve pt 1

--- src/day-08/solution.ts
+++ src/day-08/solution.ts
@@ -3,13 +3,34 @@ interface Coord {
   y: number
 }
 type Vector = Coord
+type Lookup = Record<string, Coord[]>
 
-export function solvePt1(input: string): any {
+export function solvePt1(input: string): number {
   const parsed = parseFile(input)
+  const lookup = mapToLookup(parsed)
+  const allPairs = getAllPairs(lookup)
+  const antinodes = allPairs.flatMap(([a, b]) => getAntinodesBetween(parsed, a, b))
+  const unique = uniqueCoordinates(antinodes)
+  return unique.length
 }
 
 export function solvePt2(input: string): any {
   const parsed = parseFile(input)
+}
+
+export function uniqueCoordinates(coords: Coord[]): Coord[] {
+  const strings = coords.map((c) => `${c.x}/${c.y}`)
+  const set = new Set(strings)
+  return Array.from(set).map((s) => {
+    const [x, y] = s.split('/')
+    return { x: Number(x), y: Number(y) }
+  })
+}
+
+export function getAllPairs(lookup: Lookup): [Coord, Coord][] {
+  return Object.keys(lookup)
+    .map((freq) => getPairsOfSameFreq(lookup[freq]))
+    .flatMap((pairsPer) => pairsPer)
 }
 
 export function getPairsOfSameFreq(sameFreqAntennas: Coord[]): [Coord, Coord][] {
@@ -27,8 +48,6 @@ export function getPairsOfSameFreq(sameFreqAntennas: Coord[]): [Coord, Coord][] 
 export function getAntinodesBetween(map: string[][], a: Coord, b: Coord): Coord[] {
   const vectorPlus = vectorBetween(b, a)
   const vectorMinus = vectorBetween(a, b)
-  console.log('vectorPlus', vectorPlus)
-  console.log('vectorMinus', vectorMinus)
   const coords = [applyVector(a, vectorPlus), applyVector(b, vectorMinus)]
   // filter out off board coords
   return coords.filter((c) => map[c.y]?.[c.x] != null)
@@ -42,8 +61,8 @@ export function vectorBetween(a: Coord, b: Coord): Vector {
   return { x: b.x - a.x, y: b.y - a.y }
 }
 
-export function mapToLookup(map: string[][]): Record<string, Coord[]> {
-  const result: Record<string, Coord[]> = {}
+export function mapToLookup(map: string[][]): Lookup {
+  const result: Lookup = {}
   for (let y = 0; y <= map.length - 1; y += 1) {
     for (let x = 0; x <= map[0].length - 1; x += 1) {
       const cell = map[y][x]
--- src/day-08/test.ts
+++ src/day-08/test.ts
@@ -1,6 +1,13 @@
 import { describe, it } from 'node:test'
 import assert from 'node:assert'
-import { getPairsOfSameFreq, getAntinodesBetween, mapToLookup, parseFile } from './solution'
+import {
+  getPairsOfSameFreq,
+  getAntinodesBetween,
+  mapToLookup,
+  parseFile,
+  getAllPairs,
+  solvePt1,
+} from './solution'
 import { InputReader } from '../utils/InputReader'
 import consola from 'consola'
 
@@ -8,7 +15,7 @@ describe('day-08', async () => {
   const reader = new InputReader(__dirname)
   const { inputExample, inputReal } = await reader.readAllInputFiles()
 
-  const map = [
+  const exampleMap = [
     ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
     ['.', '.', '.', '.', '.', '.', '.', '.', '0', '.', '.', '.'],
     ['.', '.', '.', '.', '.', '0', '.', '.', '.', '.', '.', '.'],
@@ -22,29 +29,30 @@ describe('day-08', async () => {
     ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
     ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
   ]
+  const exampleLookup = {
+    0: [
+      { x: 8, y: 1 },
+      { x: 5, y: 2 },
+      { x: 7, y: 3 },
+      { x: 4, y: 4 },
+    ],
+    A: [
+      { x: 6, y: 5 },
+      { x: 8, y: 8 },
+      { x: 9, y: 9 },
+    ],
+  }
 
   describe('helpers', () => {
     it('parseFile()', () => {
       const result = parseFile(inputExample)
-      assert.deepEqual(result, map)
+      assert.deepEqual(result, exampleMap)
     })
     it('mapToLookup()', () => {
-      assert.deepEqual(mapToLookup(map), {
-        0: [
-          { x: 8, y: 1 },
-          { x: 5, y: 2 },
-          { x: 7, y: 3 },
-          { x: 4, y: 4 },
-        ],
-        A: [
-          { x: 6, y: 5 },
-          { x: 8, y: 8 },
-          { x: 9, y: 9 },
-        ],
-      })
+      assert.deepEqual(mapToLookup(exampleMap), exampleLookup)
     })
     it('getAntinodesBetween()', () => {
-      assert.deepEqual(getAntinodesBetween(map, { x: 4, y: 3 }, { x: 5, y: 5 }), [
+      assert.deepEqual(getAntinodesBetween(exampleMap, { x: 4, y: 3 }, { x: 5, y: 5 }), [
         { x: 3, y: 1 },
         { x: 6, y: 7 },
       ])
@@ -74,20 +82,20 @@ describe('day-08', async () => {
     })
   })
 
-  // describe('part 1', () => {
-  //   it('example data', () => {
-  //     const result = solvePt1(inputExample)
-  //     const expected = undefined
-  //     assert.strictEqual(result, expected)
-  //   })
+  describe('part 1', () => {
+    it('example data', () => {
+      const result = solvePt1(inputExample)
+      const expected = 14
+      assert.strictEqual(result, expected)
+    })
 
-  //   // it('real data', () => {
-  //   //   const result = solvePt1(inputReal)
-  //   //   consola.success(`=== Result pt. 1: ${result} ===`)
-  //   //   const expected = undefined
-  //   //   assert.strictEqual(result, expected)
-  //   // })
-  // })
+    it('real data', () => {
+      const result = solvePt1(inputReal)
+      consola.success(`=== Result pt. 1: ${result} ===`)
+      const expected = 367
+      assert.strictEqual(result, expected)
+    })
+  })
 
   // describe('part 2', () => {
   //   it('example data', () => {
