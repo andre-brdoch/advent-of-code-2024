f90496203e853f0630af33f42d2ce06bacfe940a
andre.brdoch <31482129+andre-brdoch@users.noreply.github.com>
2024-12-08 21:02:13 +0100

    feat: Get lookup map

--- src/day-08/solution.ts
+++ src/day-08/solution.ts
@@ -1,3 +1,8 @@
+interface Coord {
+  x: number
+  y: number
+}
+
 export function solvePt1(input: string): any {
   const parsed = parseFile(input)
 }
@@ -6,6 +11,19 @@ export function solvePt2(input: string): any {
   const parsed = parseFile(input)
 }
 
-export function parseFile(file: string): any {
+export function mapToLookup(map: string[][]): Record<string, Coord[]> {
+  const result: Record<string, Coord[]> = {}
+  for (let y = 0; y <= map.length - 1; y += 1) {
+    for (let x = 0; x <= map[0].length - 1; x += 1) {
+      const cell = map[y][x]
+      if (cell === '.') continue
+      if (!(cell in result)) result[cell] = []
+      result[cell].push({ x, y })
+    }
+  }
+  return result
+}
+
+export function parseFile(file: string): string[][] {
   return file.split('\n').map((line) => line.split(''))
 }
