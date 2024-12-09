746449da103971734c9ff51da6a282f0e821e5db
andre.brdoch <31482129+andre-brdoch@users.noreply.github.com>
2024-12-09 07:24:15 +0100

    perf: Try to optimize (WIP)

--- src/day-09/solution.ts
+++ src/day-09/solution.ts
@@ -24,22 +24,38 @@ export function getChecksum(moved: string): number {
 
 export function moveBlocks(unCondensed: string): string {
   let result: string = unCondensed
+  let leftIndexLatest = 0
+  let rightIndexLatest = result.length - 1
   all: while (true) {
-    const leftIndex = result.indexOf('.')
-    let rightIndex: number = leftIndex
+    let leftIndex = leftIndexLatest
+    let rightIndex = rightIndexLatest
+    left: for (let i = leftIndexLatest; i <= rightIndexLatest; i += 1) {
+      const char = result[i]
+      const isFree = char === '.'
+      if (isFree) {
+        console.log('found left', i, 'right', rightIndexLatest)
+
+        leftIndex = i
+        leftIndexLatest = i
+
+        break left
+      } else if (i >= rightIndex) break left
+    }
     if (leftIndex === -1) {
       break all
     }
-    single: for (let i = result.length - 1; i >= 0 && i >= leftIndex; i -= 1) {
+    right: for (let i = rightIndexLatest; i >= 0 && i >= leftIndex; i -= 1) {
       const char = result[i]
+      console.log('irght', i, char)
       const isFree = char === '.'
       if (!isFree) {
         rightIndex = i
-        break single
-      }
+        rightIndexLatest = i
+        console.log('found right', i, 'left', leftIndexLatest)
+        break right
+      } else if (i <= leftIndex) break
     }
-
-    if (leftIndex === rightIndex) {
+    if (leftIndex >= rightIndex) {
       break all
     }
     let newResult = replaceCharAt(result, leftIndex, result[rightIndex])
