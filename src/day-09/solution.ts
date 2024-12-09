490162cdc76b382a5e2e3c275e510ef8bce03c71
andre.brdoch <31482129+andre-brdoch@users.noreply.github.com>
2024-12-09 10:47:02 +0100

    feat: Solve pt 2

--- src/day-09/solution.ts
+++ src/day-09/solution.ts
@@ -4,24 +4,58 @@ export function solvePt1(input: string): number {
   const parsed = parseFile(input)
   const unCondensed = unCondense(parsed)
   const moved = moveBlocks(unCondensed)
-  const checksum = getChecksum(moved)
+  const checksum = getChecksum(moved, true)
   return checksum
 }
 
-export function solvePt2(input: string): any {
+export function solvePt2(input: string): number {
   const parsed = parseFile(input)
+  const unCondensed = unCondense(parsed)
+  const moved = moveFiles(unCondensed)
+  const checksum = getChecksum(moved, false)
+  return checksum
 }
 
-export function getChecksum(moved: Spot[]): number {
+export function getChecksum(moved: Spot[], breakIfEmpty: boolean): number {
   let result = 0
   for (let i = 0; i <= moved.length - 1; i += 1) {
     const n = moved[i]
     const isFree = n === '.'
-    if (isFree) break
-    const add = Number(n) * i
+    if (isFree) {
+      if (breakIfEmpty) break
+      else continue
+    }
+    const add = n * i
     result += add
   }
   return result
+}
+
+export function moveFiles(unCondensed: Spot[]): Spot[] {
+  let result: Spot[] = unCondensed
+  let nextRightIndex: number | undefined
+  let isDone = false
+
+  let lastId = getLastId(unCondensed)
+  while (!isDone) {
+    const [newResult, rightIndex, newIsDone] = moveFile(result, nextRightIndex, lastId)
+    result = newResult
+    nextRightIndex = rightIndex
+    isDone = newIsDone
+    lastId -= 1
+  }
+  return result
+}
+
+function getLastId(unCondensed: Spot[]): number {
+  for (let i = unCondensed.length - 1; i >= 0; i -= 1) {
+    const n = unCondensed[i]
+    const isFree = n === '.'
+    if (!isFree) {
+      return Number(n)
+    }
+  }
+  throw new Error('all are free')
 }
 
 export function moveBlocks(unCondensed: Spot[]): Spot[] {
@@ -33,6 +67,61 @@ export function moveBlocks(unCondensed: Spot[]): Spot[] {
     isDone = newIsDone
   }
   return result
+}
+
+export function moveFile(
+  unCondensed: Spot[],
+  lastRightIndex?: number,
+  lastId?: number
+): [result: Spot[], lastRightIndex: number, isDone: boolean] {
+  const result = [...unCondensed]
+  let rightStart: number | undefined
+  let rightEnd: number | undefined
+  let spot: Spot | undefined
+  for (let i = lastRightIndex ?? unCondensed.length - 1; i >= 0; i -= 1) {
+    const char = unCondensed[i]
+    const isFree = char === '.'
+    if (spot != null && char !== spot) {
+      break
+    } else if (!isFree && (lastId == null || char <= lastId)) {
+      if (rightEnd == null) rightEnd = i
+      rightStart = i
+      spot = char
+    }
+  }
+  if (rightStart == null || rightEnd == null) throw new Error('right is missing')
+  const rightSize = rightEnd - rightStart + 1
+  let leftStart: number | undefined
+  let leftEnd: number | undefined
+  for (let i = 0; i <= (rightStart ?? unCondensed.length - 1); i += 1) {
+    const char = unCondensed[i]
+    const isFree = char === '.'
+    if (isFree) {
+      if (leftStart == null) leftStart = i
+      leftEnd = i
+    } else if (leftStart != null && leftEnd != null) {
+      const leftSize = leftEnd - leftStart + 1
+      const fits = leftSize >= rightSize
+      if (fits) {
+        break
+      } else {
+        leftStart = undefined
+        leftEnd = undefined
+      }
+    } else {
+      leftStart = undefined
+      leftEnd = undefined
+    }
+  }
+  if (leftStart == null || leftEnd == null) {
+    const nextRightStart = rightStart - 1
+    return [unCondensed, nextRightStart, nextRightStart < 0]
+  }
+
+  const emptySpots = Array.from(Array(rightSize).keys()).map(() => '.' as const)
+  const removed = result.splice(rightStart, rightSize, ...emptySpots)
+  result.splice(leftStart, rightSize, ...removed)
+  return [result, rightStart - 1, false]
 }
 
 export function moveBlock(unCondensed: Spot[]): [result: Spot[], isDone: boolean] {
--- src/day-09/test.ts
+++ src/day-09/test.ts
@@ -4,8 +4,11 @@ import {
   arrayToString,
   moveBlock,
   moveBlocks,
+  moveFile,
+  moveFiles,
   parseFile,
   solvePt1,
+  solvePt2,
   Spot,
   stringToArray,
   unCondense,
@@ -62,11 +65,69 @@ describe('day-09', async () => {
       assert.strictEqual(arrayToString(r6[0]), '022111222......')
       assert.strictEqual(r6[1], true)
     })
+    it('moveFile()', () => {
+      // moving id 9
+      const [result1, lastRight1, isDone1] = moveFile(unCondensedExample)
+      assert.strictEqual(arrayToString(result1), '0099.111...2...333.44.5555.6666.777.8888..')
+      assert.strictEqual(lastRight1, 39)
+      assert.strictEqual(isDone1, false)
+      // can't move id 8
+      const [result2, lastRight2, isDone2] = moveFile(result1, lastRight1, 9)
+      assert.strictEqual(arrayToString(result2), arrayToString(result1))
+      assert.strictEqual(lastRight2, 35)
+      assert.strictEqual(isDone2, false)
+      // moving id 7
+      const [result3, lastRight3, isDone3] = moveFile(result2, lastRight2, 8)
+      assert.strictEqual(arrayToString(result3), '0099.1117772...333.44.5555.6666.....8888..')
+      assert.strictEqual(lastRight3, 31)
+      assert.strictEqual(isDone3, false)
+      // can't move id 6
+      const [result4, lastRight4, isDone4] = moveFile(result3, lastRight3, 7)
+      assert.strictEqual(arrayToString(result4), arrayToString(result3))
+      assert.strictEqual(lastRight4, 26)
+      assert.strictEqual(isDone4, false)
+      // can't move id 5
+      const [result5, lastRight5, isDone5] = moveFile(result4, lastRight4, 6)
+      assert.strictEqual(arrayToString(result5), arrayToString(result4))
+      assert.strictEqual(lastRight5, 21)
+      assert.strictEqual(isDone5, false)
+      // moving id 4
+      const [result6, lastRight6, isDone6] = moveFile(result5, lastRight5, 5)
+      assert.strictEqual(arrayToString(result6), '0099.111777244.333....5555.6666.....8888..')
+      assert.strictEqual(lastRight6, 18)
+      assert.strictEqual(isDone6, false)
+      // can't move id 3
+      const [result7, lastRight7, isDone7] = moveFile(result6, lastRight6, 4)
+      assert.strictEqual(arrayToString(result7), arrayToString(result6))
+      assert.strictEqual(lastRight7, 14)
+      assert.strictEqual(isDone7, false)
+      // moving id 2
+      const [result8, lastRight8, isDone8] = moveFile(result7, lastRight7, 3)
+      assert.strictEqual(arrayToString(result8), '00992111777.44.333....5555.6666.....8888..')
+      assert.strictEqual(lastRight8, 10)
+      assert.strictEqual(isDone8, false)
+      // can't move id 1
+      const [result9, lastRight9, isDone9] = moveFile(result8, lastRight8, 2)
+      assert.strictEqual(arrayToString(result9), arrayToString(result8))
+      assert.strictEqual(lastRight9, 4)
+      assert.strictEqual(isDone9, false)
+      // can't move id 0
+      const [result10, lastRight10, isDone10] = moveFile(result9, lastRight9, 1)
+      assert.strictEqual(arrayToString(result10), arrayToString(result9))
+      assert.strictEqual(lastRight10, -1)
+      assert.strictEqual(isDone10, true)
+    })
     it('moveBlocks()', () => {
       assert.deepEqual(moveBlocks(unCondensedMini), stringToArray('022111222......'))
       assert.deepEqual(
-        moveBlocks(unCondensedExample),
-        stringToArray('0099811188827773336446555566..............')
+        arrayToString(moveBlocks(unCondensedExample)),
+        '0099811188827773336446555566..............'
+      )
+    })
+    it('moveFiles()', () => {
+      assert.deepEqual(
+        arrayToString(moveFiles(unCondensedExample)),
+        '00992111777.44.333....5555.6666.....8888..'
       )
     })
   })
@@ -85,18 +146,18 @@ describe('day-09', async () => {
     })
   })
 
-  // describe('part 2', () => {
-  //   it('example data', () => {
-  //     const result = solvePt2(inputExample)
-  //     const expected = undefined
-  //     assert.strictEqual(result, expected)
-  //   })
+  describe('part 2', () => {
+    it('example data', () => {
+      const result = solvePt2(inputExample)
+      const expected = 2858
+      assert.strictEqual(result, expected)
+    })
 
-  //   // it('real data', () => {
-  //   //   const result = solvePt2(inputReal)
-  //   //   consola.success(`=== Result pt. 2: ${result} ===`)
-  //   //   const expected = undefined
-  //   //   assert.strictEqual(result, expected)
-  //   // })
-  // })
+    it('real data', () => {
+      const result = solvePt2(inputReal)
+      consola.success(`=== Result pt. 2: ${result} ===`)
+      const expected = 6289564433984
+      assert.strictEqual(result, expected)
+    })
+  })
 })
