ed2b16409895c7ac82aa945a2fa89dd6c372a5ca
andre.brdoch <31482129+andre-brdoch@users.noreply.github.com>
2024-12-09 06:09:31 +0100

    feat: Parse file

---
+++ src/day-09/solution.ts
@@ -0,0 +1,11 @@
+export function solvePt1(input: string): any {
+  const parsed = parseFile(input)
+}
+
+export function solvePt2(input: string): any {
+  const parsed = parseFile(input)
+}
+
+export function parseFile(file: string): number[] {
+  return file.split('').map((str) => Number(str))
+}
---
+++ src/day-09/test.ts
@@ -0,0 +1,48 @@
+import { describe, it } from 'node:test'
+import assert from 'node:assert'
+import { parseFile } from './solution'
+import { InputReader } from '../utils/InputReader'
+import consola from 'consola'
+
+describe('day-09', async () => {
+  const reader = new InputReader(__dirname)
+  const { inputExample, inputReal } = await reader.readAllInputFiles()
+
+  describe('helpers', () => {
+    it('parseFile()', () => {
+      const result = parseFile(inputExample)
+      const expected = [2, 3, 3, 3, 1, 3, 3, 1, 2, 1, 4, 1, 4, 1, 3, 1, 4, 0, 2]
+      assert.deepEqual(result, expected)
+    })
+  })
+
+  // describe('part 1', () => {
+  //   it('example data', () => {
+  //     const result = solvePt1(inputExample)
+  //     const expected = undefined
+  //     assert.strictEqual(result, expected)
+  //   })
+
+  //   // it('real data', () => {
+  //   //   const result = solvePt1(inputReal)
+  //   //   consola.success(`=== Result pt. 1: ${result} ===`)
+  //   //   const expected = undefined
+  //   //   assert.strictEqual(result, expected)
+  //   // })
+  // })
+
+  // describe('part 2', () => {
+  //   it('example data', () => {
+  //     const result = solvePt2(inputExample)
+  //     const expected = undefined
+  //     assert.strictEqual(result, expected)
+  //   })
+
+  //   // it('real data', () => {
+  //   //   const result = solvePt2(inputReal)
+  //   //   consola.success(`=== Result pt. 2: ${result} ===`)
+  //   //   const expected = undefined
+  //   //   assert.strictEqual(result, expected)
+  //   // })
+  // })
+})
